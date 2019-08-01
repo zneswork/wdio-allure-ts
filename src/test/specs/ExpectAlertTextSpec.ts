1fcdd502142804d0b14c21ccbfc86fe20914815c
Felix Zilber <felix@cloudinary.com>
2019-08-01 10:16:13 +0300

    update test

--- src/test/specs/ExpectAlertTextSpec.ts
+++ src/test/specs/ExpectAlertTextSpec.ts
@@ -6,9 +6,6 @@ const TEST_FIELD_SELECTOR: string = "//*[@id='ExpectAlertText']";
 const TRIGGER_ALERT_BUTTON_SELECTOR: string = `${TEST_FIELD_SELECTOR}//button[@id='ExpectAlertTextTriggerAlert']`;
 
 describeCommon('expectAlertText', () => {
-  beforeEach(() => {
-    browser.refresh();
-  });
   it('correct text', () => {
     $(TRIGGER_ALERT_BUTTON_SELECTOR).click();
     expect(() => BrowserUtils.expectAlertText('Hello! I am an alert box!')).to.not.throw();
@@ -18,18 +15,18 @@ describeCommon('expectAlertText', () => {
     expect(() => BrowserUtils.expectAlertText('Hello! I am not alert box!'))
       .to.throw(Error)
       .with.property('message')
-      .contains("Incorrect alert's text or alert not found.");
+      .contains("Incorrect alert's text");
   });
 
   it('no alert', () => {
     expect(() => BrowserUtils.expectAlertText('Hello! I am an alert box!'))
       .to.throw(Error)
       .with.property('message')
-      .contains("Incorrect alert's text or alert not found.");
+      .contains('Alert not found');
   });
   afterEach(() => {
     try {
-      browser.dismissAlert();
+      browser.acceptAlert();
     } catch {
       console.log('no alert opened');
     } //some test does not open alert
